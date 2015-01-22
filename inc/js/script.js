0da1b3da0e09d564ce59091d444e8baaf6528e3c
Blair Wigley <blair@blairwigley.com>
2015-01-23 10:52:16 +1100

    changing from js to php - currently broken, need to fix regular expression function in index.php

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,15 +1,16 @@
 jQuery(function($) {
 	window.addEvent("domready", function() {
-		var loadgif = "//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif";
-		$("img").each(function() {
-			var $img = $(this);
-	    	$img.attr("data-src", $img.attr("src"));
-	    	$img.removeAttr("src");
-	    	$img.attr("src", loadgif);
-		});
+		//var loadgif = "//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif";
+		//$("img").each(function() {
+		//	var $img = $(this);
+	    //	$img.attr("data-src", $img.attr("src"));
+	    //	$img.removeAttr("src");
+	    //	$img.attr("src", loadgif);
+		//});
 		
 		var lazyloader = new LazyLoad({
 		});
 	});
 
 });
+
--- index.php
+++ index.php
@@ -26,4 +26,20 @@ function lazyload_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'lazyload_scripts' );
 
+
+<i
+
+// add shadowbox to images
+
+add_filter('the_content', 'lazyload_change', 12);
+
+add_filter('get_comment_text', 'lazyload_change');
+function lazyload_change ($content)
+{   global $post;
+	$pattern = "/<a(.*?)href=('|\")([^>]*).(bmp|gif|jpeg|jpg|png)('|\")(.*?)>(.*?)<\/a>/i";
+    $replacement = '<a$1href=$2$3.$4$5 data-rel="" rel="//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif" $6>$7</a>';
+    $content = preg_replace($pattern, $replacement, $content);
+    return $content;
+}
+
 ?>
