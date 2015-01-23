c580d337b4ea043012cd02331108095baedeb494
Blair Wigley <blair@blairwigley.com>
2015-01-23 13:36:55 +1100

    using regular expressions to handle changing image format - almost working

--- index.php
+++ index.php
@@ -10,6 +10,21 @@ defined('ABSPATH') or die("..");
     */
 
 
+// add lazyload to images
+function lazyload_change ($content)
+{   global $post;
+    $pattern = "/\<img (.*).*src=\"(.*\.jpg|gif|png|svg)\"(.*)\/?\>/";
+    $replacement = '<img $1data-src="$2" src="//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif"$3/>';
+    $content = preg_replace($pattern, $replacement, $content);
+    return $content;
+}
+
+add_filter('the_content', 'lazyload_change', 12);
+
+add_filter('get_comment_text', 'lazyload_change');
+
+
+//register and enqueue scripts
 function lazyload_scripts() {
 	if (!is_admin()){
 		wp_register_script('mootools', '//ajax.googleapis.com/ajax/libs/mootools/1.3.2/mootools.js', array(), null, 'all' );
@@ -25,21 +40,5 @@ function lazyload_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'lazyload_scripts' );
-
-
-<i
-
-// add shadowbox to images
-
-add_filter('the_content', 'lazyload_change', 12);
-
-add_filter('get_comment_text', 'lazyload_change');
-function lazyload_change ($content)
-{   global $post;
-	$pattern = "/<a(.*?)href=('|\")([^>]*).(bmp|gif|jpeg|jpg|png)('|\")(.*?)>(.*?)<\/a>/i";
-    $replacement = '<a$1href=$2$3.$4$5 data-rel="" rel="//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif" $6>$7</a>';
-    $content = preg_replace($pattern, $replacement, $content);
-    return $content;
-}
 
 ?>
