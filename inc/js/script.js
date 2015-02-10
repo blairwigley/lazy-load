2b8be88052f2b7fc8c6bac1ad786ce9c58e34f4c
Blair Wigley <blair@blairwigley.com>
2015-02-11 08:50:34 +1100

    removing old code froms script.js

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,13 +1,5 @@
 jQuery(function($) {
 	window.addEvent("domready", function() {
-		//var loadgif = "//cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif";
-		//$("img").each(function() {
-		//	var $img = $(this);
-	    //	$img.attr("data-src", $img.attr("src"));
-	    //	$img.removeAttr("src");
-	    //	$img.attr("src", loadgif);
-		//});
-		
 		var lazyloader = new LazyLoad({
 		});
 	});
